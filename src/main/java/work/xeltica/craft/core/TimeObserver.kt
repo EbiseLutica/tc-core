125ef50c34905ce160d9cfe14d3ca676f8c5b65f
Ebise Lutica <7106976+EbiseLutica@users.noreply.github.com>
2023-02-13 18:29:56 +0900

    TimeObserver が動作しない問題を修正

--- src/main/java/work/xeltica/craft/core/TimeObserver.kt
+++ src/main/java/work/xeltica/craft/core/TimeObserver.kt
@@ -14,7 +14,7 @@ import java.time.LocalDateTime
 class TimeObserver : BukkitRunnable() {
     override fun run() {
         val pm = Bukkit.getPluginManager()
-        val currentGameTime = Bukkit.getWorld("main")!!.time
+        val currentGameTime = Bukkit.getWorld("world")!!.time
         val currentRealTime = LocalDateTime.now()
 
         // 時間が前よりも小さくなったのであれば、おそらく日をまたいだことになる
